121a69f28ad28a4212e1da837527acbf54500a3c
Mariafmm <mariasillita28@gmai.com>
2022-05-31 23:51:52 -0500

    seeders con problemas

---
+++ database/seeders/LibrosPresentacion.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Farcades\DB;
+
+class LibrosPresentacion extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       
+
+    //     $LibrosFalsos = [
+    //         ['Titulo' => 'El conejito soñador',
+    //         'Libro' => 'Había una vez un conejito soñador que vivía en una casita en medio del bosque, rodeado de libros y fantasía, pero no tenía amigos. Todos le habían dado de lado porque se pasaba el día contando historias imaginarias sobre hazañas caballerescas, aventuras submarinas y expediciones extraterrestres. Siempre estaba inventando aventuras como si las hubiera vivido de verdad, hasta que sus amigos se cansaron de escucharle y acabó quedándose solo.',
+    //         'id_users'=> '1'
+    //     ],
+    //     [
+    //         'Titulo' => 'El conejito soñador',
+    //         'Libro' => 'Había una vez un conejito soñador que vivía en una casita en medio del bosque, rodeado de libros y fantasía, pero no tenía amigos. Todos le habían dado de lado porque se pasaba el día contando historias imaginarias sobre hazañas caballerescas, aventuras submarinas y expediciones extraterrestres. Siempre estaba inventando aventuras como si las hubiera vivido de verdad, hasta que sus amigos se cansaron de escucharle y acabó quedándose solo.',
+    //         'id_users'=> '1'
+    //     ],
+    // ];
+        
+    //     DB::table('libros')->insert($LibrosFalsos);
+    }
+}
---
+++ database/seeders/PersonasPresentacion.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Farcades\DB;
+
+class PersonasPresentacion extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //  $PersonasFalsas=[
+        //     'id'=>'1',
+        //     'nombre'=> 'prueba',
+        //     'email'=> 'prueba@gmail.com',
+        //     'password'=> 'prueba1'
+        // ];
+        // DB::table('users')->insert($PersonasFalsas);
+    }
+}
